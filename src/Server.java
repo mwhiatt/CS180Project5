b8b49e2e6fd8d52238309d0a97f08d3bbcd05b8b
mwhiatt <89279653+mwhiatt@users.noreply.github.com>
2021-12-01 12:28:58 -0500

    Skeleton for server class

--- src/Server.java
+++ src/Server.java
@@ -1,1 +1,37 @@
 
+import java.net.ServerSocket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(4242);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		while (true) {
+			Socket socket = null;
+			try {
+				String message = "";
+				socket = serverSocket.accept();
+				BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter pw = new PrintWriter(socket.getOutputStream());
+				String line = bfr.readLine();
+				while (line != null) {
+					message += line;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
