697e0d0d4391430e17f8a64be0ade67f25167987
mwhiatt <89279653+mwhiatt@users.noreply.github.com>
2021-12-10 10:57:34 -0500

    Lots of newline characters, debugging print statements

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.net.ServerSocket;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -24,11 +25,13 @@ public class Server implements Runnable {
 			PrintWriter pw = new PrintWriter(socket.getOutputStream());
 			BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			//handling
-			String message = "Not Blank";
+			String message = bfr.readLine();
 			while (message != null) {
-				message = bfr.readLine();
+				System.out.println("Entered Method handling");				
+				System.out.println("Message received from client: " + message);
 				ArrayList<String> arguments = parseMessage(message);
 				String method = arguments.get(0);
+				System.out.println("Method:" + method);
 				if (method.equals("CREATECOURSE")) {
 					
 				} else if (method.equals("DELETECOURSE")) {
@@ -48,7 +51,7 @@ public class Server implements Runnable {
 						courseStr += courses[i] + "|";
 					}
 					String ret = courseStr.substring(0, courseStr.length() - 1);
-					pw.write(ret);
+					pw.write(ret + "\n");
 					pw.flush();
 				} else if (method.equals("PRINTQUIZZES")) {
 					String[] quizzes = Teacher.printQuizzes(arguments.get(1));
@@ -57,7 +60,7 @@ public class Server implements Runnable {
 						quizStr += quizzes[i] + "|";
 					}
 					String ret = quizStr.substring(0, quizStr.length() - 1);
-					pw.write(ret);
+					pw.write(ret + "\n");
 					pw.flush();
 				} else if (method.equals("PRINTQUIZZES2")) {
 					Teacher.printQuizzes(arguments.get(1));
@@ -68,16 +71,16 @@ public class Server implements Runnable {
 						submissionsStr += submissions.get(i) + "|";
 					}
 					String ret = submissionsStr.substring(0, submissionsStr.length() - 1);
-					pw.write(ret);
+					pw.write(ret + "\n");
 					pw.flush();
 				} else if (method.equals("CHECKSUBMISSION")) {
 					
 				} else if (method.equals("CHECKQUIZ")) {
 					if (Teacher.checkQuizExistence(arguments.get(1), arguments.get(2))) {
-						pw.write("true");
+						pw.write("true" + "\n");
 						pw.flush();
 					} else {
-						pw.write("false");
+						pw.write("false" + "\n");
 						pw.flush();
 					}
 				} else if (method.equals("CHECKCOURSE")) {
@@ -93,7 +96,7 @@ public class Server implements Runnable {
 						pointsStr += points.get(i) + "|";
 					}
 					String ret = pointsStr.substring(0, pointsStr.length() - 1);
-					pw.write(ret);
+					pw.write(ret + "\n");
 					pw.flush();
 				} else if (method.equals("READQUIZ")) {
 					ArrayList<String> quizAndAnswers = Student.readQuiz(arguments.get(1), arguments.get(2));
@@ -102,7 +105,7 @@ public class Server implements Runnable {
 						quizAndAnswersStr += quizAndAnswers.get(i) + "|";
 					}
 					String ret = quizAndAnswersStr.substring(0, quizAndAnswersStr.length() - 1);
-					pw.write(ret);
+					pw.write(ret + "\n");
 					pw.flush();
 				} else if (method.equals("WRITEFILE")) {
 					Student.writeFile(arguments.get(1), arguments.get(2), arguments.get(3),
@@ -113,47 +116,87 @@ public class Server implements Runnable {
 					Teacher.printSubmissions(arguments.get(1), arguments.get(2));
 				} else if (method.equals("VIEWSUBMISSIONS")) {
 					String submission = Student.viewSubmissions(arguments.get(1));
-					pw.write(submission);
+					pw.write(submission + "\n");
 					pw.flush();
 				} else if (method.equals("LOGIN")) {
 					if (Login.login(arguments.get(1), arguments.get(2))) {
-						pw.write("true");
+						pw.write("true\n");
 						pw.flush();
+						System.out.println("Valid Login");
 					} else {
-						pw.write("false");
+						pw.write("false\n");
 						pw.flush();
+						System.out.println("Invalid Login");
 					}
 				} else if (method.equals("WRITENEWUSER")) {
 					Login.writeNewUser(arguments.get(1), arguments.get(2), arguments.get(3));
 				} else if (method.equals("ISDUPLICATE")) {
-					if (Login.isDuplicate(arguments.get(1))) {
-						pw.write("true");
+					String arg = arguments.get(1);
+					System.out.println(arg);
+					if (Login.isDuplicate(arg)) {
+						System.out.println("About to write");
+						pw.write("true" + "\n");
 						pw.flush();
+						System.out.println("Written");
 					} else {
-						pw.write("false");
+						pw.write("false" + "\n");
 						pw.flush();
 					}
 				} else if (method.equals("GETCLASSIFICATION")) {
-					pw.write(Login.getClassification(arguments.get(1)));
+					String classification = Login.getClassification(arguments.get(1));
+					System.out.println("Classification: " + classification);
+					pw.write(classification + "\n");
+					pw.flush();
 				}
+				message = bfr.readLine();
+				if (message != null) {
+					System.out.println("Message: " + message);
+				} else
+					System.out.println("Message is null");
 			}
 			
 			pw.close();
 			bfr.close();
+			socket.close();
+			System.out.println("Connection Closed.\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	public static void main(String[] args) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(4343);
-		
-		while (true) {
-			Socket socket = serverSocket.accept();
-			Server server = new Server(socket);
-			new Thread(server).start();
+	public static void main(String[] args) {
+		//Creates Login File 
+		//Creates Login File 
+		File logins = new File("login.txt");
+		if (!logins.exists()) {
+			try {
+				logins.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+				
+		//Creates coursenames file
+		File courseNames = new File("CourseNames.txt");
+		if (!courseNames.exists()) {
+			try {
+				courseNames.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			ServerSocket serverSocket = new ServerSocket(4343);
+			
+			while (true) {
+				Socket socket = serverSocket.accept();
+				System.out.println("Accepted");
+				Server server = new Server(socket);
+				new Thread(server).start();
+			}
+		} catch (IOException e) {
+			System.out.println("Socket exception");
 		}
 		
-
 	}
 	
 	public static ArrayList<String> parseMessage(String message) {
