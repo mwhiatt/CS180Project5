e9faca63f71bd27c47d794ea5352130a4993ab8b
mwhiatt <89279653+mwhiatt@users.noreply.github.com>
2021-12-12 02:08:21 -0500

    createcourse fix

--- src/Client.java
+++ src/Client.java
@@ -778,10 +778,15 @@ public class Client implements ActionListener {
                     try {
                         Socket socket = new Socket(SERVERADDRESS, 4343);
                         PrintWriter pw = new PrintWriter(socket.getOutputStream());
-
+                        BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                         pw.write("CREATECOURSE|" + courseNameRequested + "\n");
                         pw.flush();
-
+                        String response = bfr.readLine();
+                        if (response.equals("success")) {
+                        	JOptionPane.showMessageDialog(null, "New Course Created!", "Course Status",
+            						JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        bfr.close();
                         socket.close();
                         pw.close();
                     } catch (IOException exception) {
