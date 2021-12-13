dc055962947084829770ec8bdc7ba3db267c5cd8
mwhiatt <89279653+mwhiatt@users.noreply.github.com>
2021-12-13 01:48:18 -0500

    FINAL Student

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,147 +8,147 @@ import java.util.ArrayList;
  * <p>
  *
  * @author Matt Hiatt, Aryan Mathur, Aniket Mohanty, and Nathan Lo
- * @version 11/15/2021
+ * @version 12/12/2021
  */
 public class Student {
 
-   //reads the quiz to the student
-   public static ArrayList<String> readQuiz (String course, String quiz) {
-       String courseQuizFileName = course + quiz + ".txt";
-       ArrayList<String> list = new ArrayList<>();
-       synchronized (Teacher.quizKeeper) {
-	       try (BufferedReader bfr = new BufferedReader(new FileReader(courseQuizFileName))) {
-	           String s = " ";
-	           String questionString = "";
-	           do {
-	               for (int c = 0; c < 5; c++) {
-	                   s = bfr.readLine();
-	                   if (s == null) {
-	                       continue;
-	                   }
-	                   questionString = questionString + s + "~";
-	               }
-	               list.add(questionString);
-	               if (s == null) {
-	                   continue;
-	               }
-	
-	               for (int c = 0; c < 2; c++) {
-	                   if (c == 0) {
-	                       s = bfr.readLine();
-	                       list.add(s); // ans
-	                   } else if (c == 1) {
-	                       s = bfr.readLine();
-	                       list.add(s); // points
-	                   }
-	               }
-	           } while (s != null);
-	       } catch (FileNotFoundException e) {
-	    	   return null;
-	       } catch (IOException e) {
-	    	   return null;
-	       }
-       }
-       list.remove(list.size() - 1);
-       return list;
-    }
-    //writes the submission file for a student
-    public static String writeFile(String course, String quiz, String user, ArrayList<String> points, ArrayList<String> answerList) {
-        String totalString;
-        int total = 0;
-        for (int y = 0; y < points.size(); y++) {
-            total = total + Integer.valueOf(points.get(y));
-        }
-        totalString = String.valueOf(total);
+	// reads the quiz to the student
+	public static ArrayList<String> readQuiz(String course, String quiz) {
+		String courseQuizFileName = course + quiz + ".txt";
+		ArrayList<String> list = new ArrayList<>();
+		synchronized (Teacher.quizKeeper) {
+			try (BufferedReader bfr = new BufferedReader(new FileReader(courseQuizFileName))) {
+				String s = " ";
+				String questionString = "";
+				do {
+					for (int c = 0; c < 5; c++) {
+						s = bfr.readLine();
+						if (s == null) {
+							continue;
+						}
+						questionString = questionString + s + "~";
+					}
+					list.add(questionString);
+					if (s == null) {
+						continue;
+					}
 
-        int i = 1;
-        int n = 1;
-        String fileName = course + quiz + user + ".txt";
-        synchronized (Teacher.submissionKeeper) {
-	        File f = new File(fileName);
-	        while (f.exists()) {
-	            i++;
-	            fileName = course + quiz + user + i + ".txt";
-	            f = new File(fileName);
-	        }
-	        try (PrintWriter pw = new PrintWriter(new FileOutputStream(fileName, false))) {
-	            pw.println("Name: " + user);
-	            for (int c = 0; c < answerList.size(); c ++) {
-	                String ans = "Student Answer: " + answerList.get(c);
-	                String point = points.get(c);
-	                String fileInput = n + ". " + ans + ", " + point;
-	                n++;
-	                pw.println(fileInput);
-	            }
-	            pw.println("Points Earned : " + totalString);
-	            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
-	            pw.println("Timestamp: " + timeStamp);
-	        } catch (FileNotFoundException e) {
-	        	return "fail";
-                
-	        }
-        }
-        synchronized (Teacher.submissionListKeeper) {
-	        String masterFileName = course + quiz + "Submissions.txt";
-	        try (PrintWriter pw = new PrintWriter(new FileOutputStream(masterFileName, true))) {
-	            pw.println(fileName);
-	            return "success";
-	        } catch (FileNotFoundException e) {
-	        	return "fail";
-	        }
-        }
-    }
+					for (int c = 0; c < 2; c++) {
+						if (c == 0) {
+							s = bfr.readLine();
+							list.add(s); // ans
+						} else if (c == 1) {
+							s = bfr.readLine();
+							list.add(s); // points
+						}
+					}
+				} while (s != null);
+			} catch (FileNotFoundException e) {
+				return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		list.remove(list.size() - 1);
+		return list;
+	}
 
-    // Displays student the submission they have chosen to view
-    public static ArrayList<String> printSubmissions(String course, String quiz, String user) throws IOException {
-        String fileName = course + quiz + "Submissions.txt";
-        ArrayList<String> userSubmissions = new ArrayList<>(); // ArrayList that holds a particular user's submissions
-        String prompt = "";
-        boolean properInput = false;
-        synchronized (Teacher.submissionListKeeper) {
-	        try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
-	            String s = bfr.readLine();
-	            while (s != null) {
-	                if (s.contains(user)) { // checks whether a submission contains username
-	                    userSubmissions.add(s); // adds it to ArrayList
-	                }
-	                s = bfr.readLine();
-	            }
+	// writes the submission file for a student
+	public static String writeFile(String course, String quiz, String user, ArrayList<String> points,
+			ArrayList<String> answerList) {
+		String totalString;
+		int total = 0;
+		for (int y = 0; y < points.size(); y++) {
+			total = total + Integer.valueOf(points.get(y));
+		}
+		totalString = String.valueOf(total);
 
-	        } catch (IOException e) {
-                throw e;
-	        }
-	        return userSubmissions;
-        }
-    }
-    public static String viewSubmissions(String requestedSubmission) throws IOException{
-        String returnString = "";
-        synchronized (Teacher.submissionKeeper) {
-            try (BufferedReader bfr = new BufferedReader(new FileReader(requestedSubmission))) {
-                String s = bfr.readLine();
-                while (s != null) {
-                    returnString = returnString + s + "~"; // prints the user submission line by line
-                    s = bfr.readLine();
-                }
-            }  catch (IOException e) {
-                throw e;
-            }
-            return returnString;
-        }
-    }
+		int i = 1;
+		int n = 1;
+		String fileName = course + quiz + user + ".txt";
+		synchronized (Teacher.submissionKeeper) {
+			File f = new File(fileName);
+			while (f.exists()) {
+				i++;
+				fileName = course + quiz + user + i + ".txt";
+				f = new File(fileName);
+			}
+			try (PrintWriter pw = new PrintWriter(new FileOutputStream(fileName, false))) {
+				pw.println("Name: " + user);
+				for (int c = 0; c < answerList.size(); c++) {
+					String ans = "Student Answer: " + answerList.get(c);
+					String point = points.get(c);
+					String fileInput = n + ". " + ans + ", " + point;
+					n++;
+					pw.println(fileInput);
+				}
+				pw.println("Points Earned : " + totalString);
+				String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+				pw.println("Timestamp: " + timeStamp);
+			} catch (FileNotFoundException e) {
+				return "fail";
+			}
+		}
+		synchronized (Teacher.submissionListKeeper) {
+			String masterFileName = course + quiz + "Submissions.txt";
+			try (PrintWriter pw = new PrintWriter(new FileOutputStream(masterFileName, true))) {
+				pw.println(fileName);
+				return "success";
+			} catch (FileNotFoundException e) {
+				return "fail";
+			}
+		}
+	}
 
-    //calculates the points obtained by a student
-    public static ArrayList<String> grading(ArrayList<String> answerList, ArrayList<String> quizAndAnswers) {
-        ArrayList<String> points = new ArrayList<>();
-        int j = 1;
-        for (int i = 0; i < answerList.size(); i++) {
-            if(answerList.get(i).equals(quizAndAnswers.get(j))) {
-                points.add(quizAndAnswers.get(j+1));
-            } else {
-                points.add("0");
-            }
-            j += 3;
-        }
-        return points;
-    }
+	// Displays student the submission they have chosen to view
+	public static ArrayList<String> printSubmissions(String course, String quiz, String user) throws IOException {
+		String fileName = course + quiz + "Submissions.txt";
+		ArrayList<String> userSubmissions = new ArrayList<>(); // ArrayList that holds a particular user's submissions
+		synchronized (Teacher.submissionListKeeper) {
+			try (BufferedReader bfr = new BufferedReader(new FileReader(fileName))) {
+				String s = bfr.readLine();
+				while (s != null) {
+					if (s.contains(user)) { // checks whether a submission contains username
+						userSubmissions.add(s); // adds it to ArrayList
+					}
+					s = bfr.readLine();
+				}
+
+			} catch (IOException e) {
+				throw e;
+			}
+			return userSubmissions;
+		}
+	}
+
+	public static String viewSubmissions(String requestedSubmission) throws IOException {
+		String returnString = "";
+		synchronized (Teacher.submissionKeeper) {
+			try (BufferedReader bfr = new BufferedReader(new FileReader(requestedSubmission))) {
+				String s = bfr.readLine();
+				while (s != null) {
+					returnString = returnString + s + "~"; // prints the user submission line by line
+					s = bfr.readLine();
+				}
+			} catch (IOException e) {
+				throw e;
+			}
+			return returnString;
+		}
+	}
+
+	// calculates the points obtained by a student
+	public static ArrayList<String> grading(ArrayList<String> answerList, ArrayList<String> quizAndAnswers) {
+		ArrayList<String> points = new ArrayList<>();
+		int j = 1;
+		for (int i = 0; i < answerList.size(); i++) {
+			if (answerList.get(i).equals(quizAndAnswers.get(j))) {
+				points.add(quizAndAnswers.get(j + 1));
+			} else {
+				points.add("0");
+			}
+			j += 3;
+		}
+		return points;
+	}
 }
